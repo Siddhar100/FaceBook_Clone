fd69b7d40c62f4e7387faa693ac7934f9d2ebf94
Siddhartha Chakraborty <siddhartha.792.hitcsecs2020@gmail.com>
2024-02-25 20:10:48 +0530

    Home Page partial ready

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,7 +1,21 @@
+from flask import Flask,request
 from app import app
 from model.user_model import user_model
 
 obj = user_model()
 @app.route('/')
-def user_sign_controller():
-    return obj.user_sign()
+def home_page():
+    return obj.user_home_page()
+
+@app.route('/signUpPage')
+def signUpPage():
+    return obj.signUpPage()
+
+@app.route('/create_account',methods=['GET','POST'])
+def user_create_account():
+    if request.method == 'POST':
+       return obj.user_create_account(request.form)
+
+@app.route('/test')
+def test():
+    return obj.test()
--- model/user_model.py
+++ model/user_model.py
@@ -1,5 +1,30 @@
+import mysql.connector
 from flask import Flask,render_template
 
+
 class user_model():
-    def user_sign(self):
+    def __init__(self):
+        try:
+            self.con = mysql.connector.connect(host='localhost',user='root',password='',database='facebook')
+            self.con.autocommit = True
+            self.cur = self.con.cursor(dictionary=True)
+            print('connection established!')
+        except:
+            print('error!')
+
+    def test(self):
+        self.cur.execute("SELECT * FROM user_data;")
+        result = self.cur.fetchall()
+        return result
+    
+    def signUpPage(self):
+        return render_template('SignUpPage.html')
+    
+    def user_home_page(self):
         return render_template('LoginPage.html')
+    
+    def user_create_account(self,data):
+        query = f"INSERT INTO user_data (mobile_no,first_name,last_name,password) values('{data['mobile_no']}','{data['first_name']}','{data['last_name']}','{data['password']}');"
+        self.cur.execute(query)
+        return render_template('LoginPage.html')
+        
