71dff80844f7a96805254000de37029af8deb86c
Siddhartha Chakraborty <siddhartha.792.hitcsecs2020@gmail.com>
2024-02-28 17:09:10 +0530

    Message view pending!

--- model/user_model.py
+++ model/user_model.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from flask import Flask,render_template,session
+from flask import Flask,render_template,session,redirect
 
 
 class user_model():
@@ -35,17 +35,41 @@ class user_model():
             session["mobile_no"] = form_mobile
             session["user_dp"] =  user_dp 
             dp =  user_dp 
-            self.cur.execute(f"SELECT * FROM user_post order by si_no asc;")
-            posts = self.cur.fetchall()
-            return render_template('HomePage.html',posts=posts,dp=dp)
+            self.cur.execute(f"select followee from followers where follower = {form_mobile} ;")
+            followees = self.cur.fetchall()
+            if len(followees) == 0:
+                return render_template('HomePage.html',followees=followees,dp=dp)
+            else:
+                include = ""
+                for followee in followees:
+                    include = include + "'" + str(followee['followee']) + "',"
+                peoples = include[:len(include)-1]
+                print(peoples)
+                self.cur.execute(f"SELECT * FROM user_post where mobile_no  in ({peoples},'{session.get('mobile_no')}');")
+                print(f"SELECT * FROM user_post where mobile_no  in ({peoples},'{session.get('mobile_no')}');")
+                posts = self.cur.fetchall()
+                return render_template('HomePage.html',posts=posts,dp=dp,followees=followees)
         else:
             return "Sorry!"
 
     def dash_board(self):
-        self.cur.execute(f"SELECT * FROM user_post order by si_no asc;")
-        posts = self.cur.fetchall()
-        dp = session.get('user_dp')
-        return render_template('HomePage.html',posts=posts,dp=dp)
+        self.cur.execute(f"SELECT * FROM user_data where mobile_no = {session.get('mobile_no')};")
+        result = self.cur.fetchall()
+        dp = result[0]['dp_url']
+        self.cur.execute(f"select followee from followers where follower = {session.get('mobile_no')} ;")
+        followees = self.cur.fetchall()
+        if len(followees) == 0:
+            return render_template('HomePage.html',followees=followees,dp=dp)
+        else:
+            include = ""
+            for followee in followees:
+                include = include + "'" + str(followee['followee']) + "',"
+            peoples = include[:len(include)-1]
+            print(peoples)
+            self.cur.execute(f"SELECT * FROM user_post where mobile_no  in ({peoples},'{session.get('mobile_no')}');")
+            print(f"SELECT * FROM user_post where mobile_no  in ({peoples},'{session.get('mobile_no')}');")
+            posts = self.cur.fetchall()
+            return render_template('HomePage.html',posts=posts,dp=dp,followees=followees)
     
     def peoples_page(self):
         self.cur.execute(f"select followee from followers where follower = '{session.get('mobile_no')}' ;")
@@ -77,16 +101,17 @@ class user_model():
         return render_template('DpPage.html')
         
     def new_post(self,file_name):
-        self.cur.execute(f"SELECT first_name,last_name from user_data where mobile_no = '{session.get('mobile_no')} ';")
+        self.cur.execute(f"SELECT first_name,last_name,mobile_no from user_data where mobile_no = '{session.get('mobile_no')} ';")
         user_info = self.cur.fetchall()
         first_name = user_info[0]['first_name']
         last_name = user_info[0]['last_name']
+        mobile = user_info[0]['mobile_no']
         dp = session.get('user_dp')
-        query = f"INSERT INTO user_post (dp_url,first_name,last_name,post_url) values('{dp}','{first_name}','{last_name}','{file_name}');"
+        query = f"INSERT INTO user_post (dp_url,first_name,last_name,post_url,mobile_no) values('{dp}','{first_name}','{last_name}','{file_name}','{mobile}');"
         self.cur.execute(query)
         self.cur.execute(f"SELECT * FROM user_post order by si_no asc;")
         posts = self.cur.fetchall()
-        return render_template('HomePage.html',posts=posts,dp=dp)
+        return redirect('/home_page')
 
     def upload_dp(self,file_location):
         self.cur.execute(f"UPDATE user_data set dp_url = '{file_location}' where dp_url = 'blank' ;")
